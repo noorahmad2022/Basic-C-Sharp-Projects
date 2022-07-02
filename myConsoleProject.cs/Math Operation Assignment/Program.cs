12e24c4245c27489336f680c0b8f58eef8dfe475
Noorahmad Noor <noornoorahmad694@gmail.com>
2022-07-02 11:00:34 -0700

    Math Operation Assignment Submission

---
+++ myConsoleProject.cs/Math Operation Assignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Operation_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter any number to multiply by 50");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int numResult = num * 50;
+            Console.WriteLine("Result = " + numResult);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter any number to add by 25");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num1Result = num1 + 25;
+            Console.WriteLine("Result = " + num1Result);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter any number to divid it by 12.5");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            double num2Result = num2 / 12.5;
+            Console.WriteLine("Result = " + num2Result);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter any number greater than 50");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            bool userResult = userNum > 50;
+            Console.WriteLine(userResult);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter any number to divides by 7 and find the remainder");
+            int user5Num = Convert.ToInt32(Console.ReadLine());
+            int dividResult = user5Num / 7;
+            Console.WriteLine("division result = " + dividResult);
+            int remainder = dividResult % 2;
+            Console.WriteLine("Remider result = " + remainder);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ myConsoleProject.cs/MathAndComparisonOperator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine("Grand Total is = " + combined);
+            //Console.ReadLine();
+
+            //int differenc = 10 - 5;
+            //Console.WriteLine("Ten minus Five = " + differenc.ToString());
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine("10 multiply by 5 = " + product);
+            //Console.ReadLine();
+
+            //int quotient = 100 / 17;
+            //Console.WriteLine("100 divided into 5 = " + quotient);
+            //Console.ReadLine();
+
+            //OR
+
+            //double quotient1 = 100.15 / 17.60;
+            //Console.WriteLine("100.15 divided into 17.60 = " + quotient1);
+            //Console.ReadLine();
+
+            // Find Even and Odd number (The modulus operator divides two numbers and returns the Remider)
+            //int remainder = 11 % 2;
+            //Console.WriteLine (remainder);
+            //Console.ReadLine();
+
+            //bool trueORfalse = 12 < 5;
+            //Console.WriteLine(trueORfalse.ToString());
+            //Console.ReadLine();
+
+            //int roomTemp = 70;
+            //int currentTemp = 70;
+            ////bool isWarm = currentTemp >= roomTemp;
+            //bool isWarm = currentTemp != roomTemp;
+            //bool isWarm = currentTemp == roomTemp;
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+
+        }
+    }
+}
