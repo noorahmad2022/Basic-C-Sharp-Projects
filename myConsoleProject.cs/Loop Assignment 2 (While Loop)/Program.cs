8167416b916f06361068f880a2ce8dab0f2137c3
Noorahmad Noor <noornoorahmad694@gmail.com>
2022-07-02 17:57:59 -0700

    Loop Assignment Submission

---
+++ myConsoleProject.cs/Loop Assignment 2 (While Loop)/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Assignment_2__While_Loop_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess my favoret number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number==100;
+
+            while (!isGuessed)
+            {
+                switch (number)
+                {
+                    case 55:
+                        Console.WriteLine("Guess my favoret number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("You guessed 55. Try Again");
+                        break;
+                    case 78:
+                        Console.WriteLine("Guess my favoret number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("You guessed 78. Try Again");
+                        break;
+                    case 100:
+                        Console.WriteLine("You guessed 100. That is my favoret number!");
+                        isGuessed = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Guess my favoret number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("You are wrong. please try again");
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- myConsoleProject.cs/Price-Quote Application Assignment/Program.cs
+++ myConsoleProject.cs/Price-Quote Application Assignment/Program.cs
@@ -20,7 +20,7 @@ namespace Price_Quote_Application_Assignment
                     Console.ReadLine();
                     return;
                 }
-            
+
             Console.WriteLine("Please enter the package width: ");
             double packageWidth = Convert.ToDouble(Console.ReadLine());
 
@@ -39,6 +39,61 @@ namespace Price_Quote_Application_Assignment
             }
             double priceQuote = (packageWidth * packageHeight * packageLength * packageWeigh) /100;
             Console.WriteLine("Your estimated total for shipping this package is: $" + priceQuote);
+            Console.ReadLine();
+
+
+        //Assignment
+            int num1 = 43;
+            int num2 = 37;
+
+            string result = num1 > num2 ? "num1 is greater than num2" : "num1 is less than num2";
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        //Asignment
+
+            int num11 = 43;
+            int num12 = 37;
+            int num13 = 68;
+            int num14 = 54;
+
+            if (num11 < num12)
+            {
+                Console.WriteLine("Statement 1");
+            }
+            else if (num11 >= num13)
+            {
+                Console.WriteLine("Statement 2");
+            }
+            else if (num13 <= num12)
+            {
+                Console.WriteLine("Statement 3");
+            }
+            else if (num11 - num12 <= num13 - num14)
+            {
+                Console.WriteLine("Statement 4");
+            }
+            Console.ReadLine();
+
+
+        //Assignment
+
+            int num01 = 43;
+            int num02 = 37;
+            int num03 = 68;
+
+            string numresult = num01 > num02 && num02 < num03 ? "statement 11111" : "statement 2222";
+            Console.WriteLine(numresult);
+            Console.ReadLine();
+
+
+        //Assignment
+            int mum1 = 43;
+            int mum2 = 37;
+            int mum3 = 68;
+
+            string mumresult = mum1 > mum2 && mum2 > mum3 ? "Statement 1 M" : "Statement 2 M";
+            Console.WriteLine(mumresult);
             Console.ReadLine();
 
         }
