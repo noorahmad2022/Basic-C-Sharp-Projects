43fcc4e78f26bf5aa5a5f72d4906427d1e9f4cf6
Noorahmad Noor <noornoorahmad694@gmail.com>
2022-07-02 20:11:06 -0700

    final

---
+++ myConsoleProject.cs/test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num11 = 43;
+            int num21 = 37;
+            int num31 = 68;
+
+            string result = num11 + num21 > num31 ? "Statement1" : "statement2";
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+
+        }
+    }
+}
