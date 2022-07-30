d0aab4f6c2110413f54a7ea2b55e610406339454
Noorahmad Noor <noornoorahmad694@gmail.com>
2022-07-30 12:37:10 -0700

    Six-part Assignment

---
+++ ArrayAndList/Iteration/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // For Loop with Int.
+        int[] testScore = { 98, 67, 70, 87 ,91, 89, 34, 60 };
+
+        for (int i = 0; i < testScore.Length; i++)
+        {
+            if (testScore[i] > 80)
+            {
+                Console.WriteLine("Passing test score: " + testScore[i]);
+            }
+        }
+        Console.ReadLine();
+
+        // For Loop with String.
+        string[] empName = { "Noorahmad", "Jesse", "Daniel", "Adam" };
+        for(int j=0; j<empName.Length; j++)
+        {
+            if(empName[j]=="Noorahmad" || empName[j] == "Jesse")
+            {
+                Console.WriteLine("Hello " +empName[j] +"!");
+            }
+        }
+        Console.ReadLine();
+
+        // For Each Loop with INT List
+
+        List<int> testScores = new List<int>();
+        testScores.Add(88);
+        testScores.Add(97);
+        testScores.Add(78);
+        testScores.Add(69);
+        testScores.Add(60);
+
+        foreach (int s in testScores)
+        {
+            if(s > 85)
+            {
+                Console.WriteLine("Passing test Score: " + s);
+            }
+        }
+        Console.ReadLine();
+
+        // For Each Loop with String List
+
+        List<String> Names = new List<string>() { "Noorahmad", "Jesse", "Daniel", "Adam" };
+        foreach(string personName in Names)
+        {
+            if(personName=="Noorahmad" || personName == "Jesse")
+            {
+                Console.WriteLine("Hello " + personName);
+            }
+        }
+        Console.ReadLine();
+
+        // Passing int to empty list and Counting the result.
+
+        List<int> examScore = new List<int>() { 98, 95, 80, 34, 89 };
+        List<int> examPassingScore = new List<int>(); // empty list
+
+        foreach(int scores in examScore)
+        {
+            if(scores > 85)
+            {
+                examPassingScore.Add(scores);
+            }
+        }
+        Console.WriteLine("Number of passing Scores : " + examPassingScore.Count);
+        Console.ReadLine();
+
+
+    }
+}
---
+++ ArrayAndList/Six-Part Assignment/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Six_Part_Assignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+        // Console App Assignment Part 1
+           Console.WriteLine("Enter any text to show at the end of each employee name?");
+           string userText = Console.ReadLine();
+           
+            string[] empName = { "Noorahmad", "Jesse", "Daniel", "Adam" };
+            for (int j = 0; j < empName.Length; j++)
+            {
+             Console.WriteLine(empName[j] + ", " + userText);
+                
+            }
+            Console.ReadLine();
+
+
+         // Console App Assignment Part 2 and 3
+         // An infinite loop is a loop that keeps running ‘forever’, 
+
+            int i = 0;
+            while (true)
+            {
+                Console.WriteLine($"i = {i}");
+                i++;
+
+                if (i < 11)
+                {
+                    //break;
+                }
+
+                else if(i>10 && i <= 15)
+                {
+                    //break
+                }
+
+                else if (i>20)
+                {
+                    break;  //if we don't write break, the loops keeps running.
+                }
+            }
+            Console.ReadLine();
+
+
+        // Console App Assignment Part 4
+
+           List<string> employees = new List<string> { "Noorahmad", "Jesse", "Daniel", "Adam" };
+
+           Console.WriteLine("Please type the employee name i.e. Noorahmad, Jesse, Daniel, and Adam to display thier ID number");
+           string userData = Convert.ToString(Console.ReadLine());
+
+            int index = employees.FindIndex(a => a.Contains($"{userData}"));
+
+            if (index >= 1)
+            {
+                Console.WriteLine("Employee ID = " + index);
+            }
+            else
+            {
+                Console.WriteLine("Employee not found in a list");
+            }
+
+            Console.ReadLine();
+
+        // Console App Assignment Part 5 and 6
+        
+            List<String> identical = new List<string>() { "Jesse", "Jesse", "Daniel", "Adam", "Noorahmad" };
+
+            Console.WriteLine("Enter an employee Name?");
+            string empNamebyUser = Console.ReadLine();
+
+            foreach (string identicalName in identical)
+            {
+                if (empNamebyUser.Contains(identicalName))
+                {
+                    Console.WriteLine(identicalName + " Found in the list");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, not found in the list");
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
