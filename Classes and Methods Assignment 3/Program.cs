6f52cf831b009393fe535b7a61842ccec4bdb08e
Noorahmad Noor <102923335+noorahmad2022@users.noreply.github.com>
2022-09-10 12:34:48 -0700

    Classes and Methods Assignment 3

---
+++ Classes and Methods Assignment 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Methods_Assignment_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass myClass = new myClass();
+
+            Console.WriteLine("Enter the First Number");
+            int userFirstNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number");
+            int userScondNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+
+            Console.WriteLine("********** Methode call by passing two number! ***************");
+            myClass.myOperation(userFirstNum, userScondNum);
+            Console.WriteLine("");
+
+            Console.WriteLine("********** Methode call specifying the parameters by name ***************");
+            myClass.myOperation(num2: userScondNum, num1: userFirstNum);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Classes and Methods Assignment 3/myClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Methods_Assignment_3
+{
+    public class myClass
+    {
+        public void myOperation(int num1, int num2)
+        {
+            int operation = num1 * 50;
+            Console.WriteLine("The first number you entered is " +num1 + " which is * by 50 " + "and the Result = " + operation);
+            Console.WriteLine("The second number you entered is  "+num2+ " and there is no operation required for this");
+
+        }
+    }
+}
